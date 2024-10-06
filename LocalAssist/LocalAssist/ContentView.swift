55981470449d49fa6f7894c619d7cebeb677f1c8
Phillip <78912067+lebrozaurjagger@users.noreply.github.com>
2024-10-06 15:19:56 +0300

    1.0.0

---
+++ LocalAssist/LocalAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalAssist
+//
+//  Created by Phillip on 06.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalAssist/LocalAssist/LocalAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAssistApp.swift
+//  LocalAssist
+//
+//  Created by Phillip on 06.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
